c627dc53b71e81af0b55a46d6d210b825031eb7f
Tetsuya Mori <t2y3141592@gmail.com>
2025-02-17 18:08:27 +0900

    [vulnerability] Error without recovery on hidden property assignment from a JSON object; Allow copying of hidden properties on cloning from the same class instance

--- src/jsonclass.js
+++ src/jsonclass.js
@@ -321,6 +321,13 @@ const JSONClass = (class JSONClass { // declaration and initialization at once w
           configurable: true,
           enumerable: false, // hidden property
         });
+        if (loading && (property in initProperties) && !(initProperties instanceof this.constructor)) {
+          // hidden properties:
+          //  - discarded as errors on loading from a JSON object regardless of recoveryMethod
+          //  - copied on cloning from an instance of the same class without errors
+          this.constructor.onError({ jsonPath, type: property, value: initProperties[property], message: "hidden property assignment" });
+          continue; // no recovery from errors
+        }
       }
       const types = parsedTypes[currentSchema] ||
         (parsedTypes[currentSchema] =
